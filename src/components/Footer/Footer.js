e66541f63a8080dd8878d13d58c646636b4374f1
Vangelis <ev.fotiades@yahoo.com>
2024-02-17 16:30:27 +0200

    completed footer component for tablet

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,13 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div class="footer">
-                <a href="#">Privacy Policy  </a> 
-                | 
-                <a href="#">  Cookie Policy  </a> 
-                | 
-                <a href="#">  Terms and Conditions</a>
+                <div class="footer__anchors">
+                    <a href="#">Privacy Policy  </a> 
+                    | 
+                    <a href="#">  Cookie Policy  </a> 
+                    | 
+                    <a href="#">  Terms and Conditions</a>
+                </div>
             <img src={footerIcon} alt="footer" />
         </div>
     );
